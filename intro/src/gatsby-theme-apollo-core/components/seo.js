e5d4458601104ee3d99cd1baaeb0b16f282a4056
AJ Roeth <aj.roeth@zapatacomputing.com>
2020-04-09 18:47:12 -0400

    Added transparent favicon but it still flashes the old one

--- intro/src/gatsby-theme-apollo-core/components/seo.js
+++ intro/src/gatsby-theme-apollo-core/components/seo.js
@@ -4,7 +4,7 @@ import {Helmet} from 'react-helmet';
 export default function SEO({title, description, siteName}) {
   return (
     <Helmet>
-      <link rel="icon" href="favicon.ico" />
+      <link rel="icon" href="orquestra-logo.ico" />
       {/* other SEO tags (OpenGraph, Twitter, etc.) */}
     </Helmet>
   );
