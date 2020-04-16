e5e341df26d1bde5ab808ef98d8b1f1b0b022165
James Ma <jama.22@gmail.com>
2020-04-15 22:35:54 -0400

    menu cleanup

--- intro/gatsby-config.js
+++ intro/gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
         sidebarCategories: {
           null: ['index','basics/platform','basics/why-workflows', 'basics/glossary'],
           Tutorial: ['tutorial/helloworkflow','tutorial/hydrogen-vqe'],
-          Resources: ['quantum_resources/integrations', 'quantum_resources/variational_loop'],
+          Resources: ['quantum-resources/interfaces', 'quantum-resources/variational_loop'],
         }
       }
     }
